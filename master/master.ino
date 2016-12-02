d1459d68852d6a140dc2da76bd09b5932a326441
akhiyarwaladi <akiyar18@gmail.com>
2016-12-02 23:18:00 +0700

    first commit

---
+++ master/master.ino
@@ -0,0 +1,24 @@
+#include <Wire.h>
+
+void setup() {
+  Wire.begin();       
+  Serial.begin(9600);  
+  pinMode(13, OUTPUT);
+}
+
+void loop() {
+  Wire.requestFrom(8, 1);    
+
+  while (Wire.available()) {
+    int c = Wire.read();
+    if (c > 10){
+      digitalWrite(13, HIGH);
+    }
+    else{
+      digitalWrite(13, LOW);
+    }
+    Serial.println(c);         
+  }
+
+  delay(1000);
+}
---
+++ slave/slave.ino
@@ -0,0 +1,22 @@
+#include <Wire.h>
+#include <NewPing.h>
+#define TRIGGER_PIN   12  
+#define ECHO_PIN      11  
+#define MAX_DISTANCE  200 
+NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
+int value = 0;
+
+void setup() {
+  Wire.begin(8);                
+  Wire.onRequest(requestEvent); 
+}
+
+void loop() {
+  delay(1000);
+  value = sonar.ping_cm();
+}
+
+
+void requestEvent() {
+  Wire.write(value);
+}
